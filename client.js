f6b61aaf9e0073690384a07125b9dd0b052b66cf
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-31 23:21:31 +0000

    Client imported Connect Fn from Play.js

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connecting ...");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+
+  return conn;
+};
+
+connect();
+
+module.exports = connect;
